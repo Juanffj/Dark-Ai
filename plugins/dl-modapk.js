a2752a45f250c428b27ff0b6e99ed29494e9f4e3
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-10 14:32:23 -0400

    Actualizar dl-modapk.js

--- plugins/dl-modapk.js
+++ plugins/dl-modapk.js
@@ -1,44 +1,22 @@
-import axios from 'axios'
-let handler = async (m, {
-   conn,
-   usedPrefix,
-   command,
-   text
-}) => {
-   try {
-      if (!text) return m.reply(`Ejemplo: ${usedPrefix + command} WhatsApp`)
-      m.reply(wait)
-      let res = await axios.get('https://api.alyachan.dev/api/apk?q='+text+'&apikey=Ariel1')
-      let json = res.data
-      if (!json.status) return m.reply(Func.jsonFormat(json))
-      let { 
-      icon, 
-      title, 
-      size, 
-      category, 
-      download, 
-      rating, 
-      url 
-      } = json.data[0]
-      let response = `
-*乂  APK - DESCARGAS 乂*
-
- `+'_*`${title}`_*'+`
- 
- 📦 : ${size}
- 🗃️ : ${category}
- ⬇️ : ${download}
- 🌟 : ${rating}
-`
-await conn.sendMessage(m.chat, {image: {url: icon }, caption: response}, {quoted: m});
-await conn.sendMessage(m.chat, {document: {url: url }, mimetype: 'application/vnd.android.package-archive', fileName: title + '.apk', caption: null}, {quoted: m});
-   } catch (e) {
-      console.log(e)
-      return conn.reply(m.chat, Func.jsonFormat(e), m)
-   }
-}
-handler.help = ['apk']
-handler.tags = ['downloader']
-handler.command = /^(apk|app)$/i
-handler.limit = 1
-export default handler
+import {search, download} from 'aptoide-scraper';
+const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
+ if (!text) throw `*INGRESA EL NOMBRE DE LA APK*\n_Ejemplo: .apk <nombre>_`;
+        m.react('✅')
+  try {    
+    const searchA = await search(text);
+    const data5 = await download(searchA[0].id);
+    let response = `*乂  APTOIDE - DESCARGAS* 乂\n\n🤍 Nombre: ${data5.name}\n🚀 𝙋𝘼𝘾𝙆𝘼𝙂𝙀: ${data5.package}\n⏳ 𝙐𝙇𝙏𝙄𝙈𝘼 𝘼𝘾𝙏..: ${data5.lastup}\n🕋 𝙏𝘼𝙈𝘼Ñ𝙊: ${data5.size}`
+    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
+      return await conn.sendMessage(m.chat, {text: '${mssg.avisoGene4}\n\n*El archivo es demasiado grande por lo cual no se enviara*'}, {quoted: m});
+    }
+    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
+  } catch {
+    throw `*Error no se encontraron resultados para su busqueda*`;
+  }    
+};
+handler.help = ["apk <name>"]
+handler.corazones = 2
+handler.tags = ["dl"]
+handler.command = /^(apk|modapk|dapk2|aptoide|aptoidedl)$/i;
+export default handler;
