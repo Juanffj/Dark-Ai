5445d45daeebb1676c9bb07560f9fdfaab626c3b
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-10 21:12:19 -0400

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -65,7 +65,7 @@ const defaultMenu = {
   header: '╭• •꒰─• `%category` ·ٜ۬･',
   body: '│ %cmd\n',
   footer: '╰• •───• •───• •───•\n',
-  after: ``,
+  after: `> BY GENESISBOT-MD`,
 }
 let ppp = 'https://i.ibb.co/4dGjmbT/file.jpg'
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
