666599acc70bc194b93d4099db25df987aa9705e
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-10 21:59:38 -0400

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -1,44 +1,38 @@
+/*Créditos a https://github.com/AzamiJs*/
+
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import fetch from 'node-fetch'
 
-let handler = async (m) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, '🍭 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
-  await m.react('🕓')
-  try {
-  let media = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
-  let img = await (await fetch(`${link}`)).buffer()
-  let txt = '`T E L E G R A P H`\n\n'
-      txt += `*🔖 Enlace* : ${link}\n`
-      txt += `*🔖 Acortado* : ${await shortUrl(link)}\n`
-      txt += `*🔖 Tamaño* : ${formatBytes(media.length)}\n`
-      txt += `*🔖 Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += `🐈‍⬛ *${textbot}*`
+var handler = async (m) => {
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw '*⚠️ RESPONDA A UNA IMAGEN*'
+m.react(done)
+let media = await q.download()
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+let link = await (isTele ? uploadImage : uploadFile)(media)
+let info = ` *🗂️ ENLACE:*\n${link}\n
+*⚖️ TAMAÑO:*\n${media.length} bytes\n
+*🚀 EXPIRACION:*\n ${isTele ? '✅ NO EXPIRA' : '⚠️ DESCONOCIDO'}\n
+*🔰 ACORTADO:*\n${await shortUrl(link)}`
+
+conn.reply(m.chat, info, m, { contextInfo: { externalAdReply :{ mediaUrl: ig, mediaType: 2, title: wm, body: des, thumbnail: await(await fetch(link)).buffer(), sourceUrl: link}}})
+
+}
 handler.help = ['tourl']
-handler.tags = ['convertir']
+handler.tags = ['transformador']
 handler.command = /^(tourl|upload)$/i
+
+handler.limit = true
+
 export default handler
 
-function formatBytes(bytes) {
-  if (bytes === 0) {
-    return '0 B';
-  }
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB']
-  const i = Math.floor(Math.log(bytes) / Math.log(1024))
-  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
-}
-
 async function shortUrl(url) {
-        let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-        return await res.text()
+let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+return await res.text()
 }
