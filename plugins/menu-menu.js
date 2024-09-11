dae26011edb7b9759fda2eb2ff226b52fe89c610
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-10 20:40:47 -0400

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -71,7 +71,7 @@ const defaultMenu = {
   footer: '╚════•.·:·.✧ ✦ ✧.·:·.*•════╝\n\n',
   after: ``,
 }
-let ppp = 'https://i.ibb.co/48TMftG/file.jpg'
+let ppp = 'https://i.ibb.co/4dGjmbT/file.jpg'
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
     let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
