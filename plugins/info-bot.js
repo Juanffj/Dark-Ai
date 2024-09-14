7f1b4862b28e13e446ad4ba76ee531f59c05defd
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-13 22:49:56 -0400

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,27 +1,17 @@
 import fs from 'fs';
 const handler = (m) => m;
 handler.all = async function(m) {
-
-const chat = global.db.data.chats[m.chat];
-if (chat.isBaneed) return
-if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🤍 ¡Hola! Soy GenesisBot-MD, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
-}
-/*if (/^que|q$/i.test(m.text)) {
-conn.reply(m.chat, `*so y arroz* 👌🤌`, m, rcanal, )*/
-}*/
-if (/^sexo$/i.test(m.text)) {
-conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
-}
-/*if (/^a$/i.test(m.text)) {
-conn.reply(m.chat, `*rroz y pollo* 😄👌`, m, rcanal, )
-}*/
-/*if (/^bug$/i.test(m.text)) {
-conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )*/
-}
-if (/^pene$/i.test(m.text)) {
-conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
-}
-return !0;
+  const vn = './src/mp3/bot.mp3';
+  const chat = global.db.data.chats[m.chat];
+  //const s = seconds: '4556'
+  // const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡', orderTitle: 'Bang', thumbnail: fs.readFileSync('./Menu2.jpg'), sellerJid: '0@s.whatsapp.net'}}}
+  // const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: {"audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"}}}
+  if (/^bot$/i.test(m.text) && !chat.isBanned) {
+    conn.sendPresenceUpdate('recording', m.chat);
+    await m.reply(`🤍 ¡Hola! Soy GenesisBot-MD, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`);
+    m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+    // conn.sendFile(m.chat, vn, 'HolaXiaBot.mp3', null, m, true, { type: 'audioMessage', seconds: '4556', ptt: true, sendEphemeral: true, quoted: m })
+  }
+  return !0;
 };
 export default handler;
