99409812566ddffe0400185f2d4ea83abd5aa181
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-13 23:05:12 -0400

    Actualizar search-twitter_post.js

--- plugins/search-twitter_post.js
+++ plugins/search-twitter_post.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, command, args, text, usedPrefix }) => {
 if (!text) return conn.reply(m.chat, '*Ingresa el texto de lo que quieres buscar en Twitter*', m);
+await m.react('🕒');
 
 try {
 let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/Twitter-Posts?text=${encodeURIComponent(text)}`);
@@ -16,8 +17,10 @@ let txt = `*Usuario* : ${info.user || 'No disponible :('}
 *Post* : ${info.post}
 *Link* : ${info.user_link}\n\n> ʙʏ : ɢᴇɴᴇꜱɪꜱ ʏ ᴀɴɢᴇʟ-ᴏꜰᴄ`;
 
+await m.react('✅');
 await conn.sendFile(m.chat, info.profile, 'thumbnail.jpg', txt, m, null, fake);
 } catch {
+await m.react('✖️');
 conn.reply(m.chat, 'Error al procesar la solicitud. Inténtalo de nuevo más tarde.', m);
 }
 };
