02b32d7951f50f9d9f2376adc2d56ac9cfc7c11b
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-13 23:51:11 -0400

    Actualizar jadibot-serbot-info.js

--- plugins/jadibot-serbot-info.js
+++ plugins/jadibot-serbot-info.js
@@ -13,9 +13,7 @@ async function handler(m, { conn: stars, usedPrefix }) {
 
   let img = await (await fetch(`https://i.ibb.co/LSBGpg4/file.jpg`)).buffer()
   let message = users.map((v, index) => `
-*[ \`${index + 1}\` -  ${v.user.name || 'Sin Nombre'} ]*\n* *🔗 \`» Link :\`*
-
- * -* @${v.user.jid.replace(/[^0-9]/g, '')}\n│❀ *Link:* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n│❀ *Nombre:* ╰─⬣`).join('\n\n')
+*[ \`${index + 1}\` -  ${v.user.name || 'Sin Nombre'} ]*\n* *🔗 \`» Link :\`* https://wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n`).join('\n\n')
 
   let replyMessage = message.length === 0 ? '' : message
   global.totalUsers = users.length
