3979253e8ac626ac26e240adedb5bba8d812a097
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-15 22:34:15 -0400

    Actualizar plugins

---
+++ plugins/search-tiktok2.js
@@ -0,0 +1,31 @@
+// By Jtxs 🐢
+
+// _[ Tiktokb Search 🔎 ]_
+import axios from 'axios';
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, '*ingresa el texto de lo que quieres buscar en tiktok*', m);
+await m.react('🕒');
+  
+
+try {
+let { data: api } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text);
+let json = api.data;
+
+let info = json[Math.floor(json.length * Math.random())];
+
+if (info) {
+await conn.sendMessage(m.chat, { video: { url: info.nowm }, caption: info.url }, { quoted: m });
+await m.react('✅');
+} else {
+conn.reply('error :v')
+}
+} catch {
+conn.reply('error :v');
+await m.react('✖️')
+}
+};
+
+handler.command = ['tiktoksearch2'];
+
+export default handler;
