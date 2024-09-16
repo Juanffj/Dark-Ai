8c087b17e026f3df22676887bd471d123ee99546
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-16 17:46:02 -0400

    Actualizar dl-imagen.js

--- plugins/dl-imagen.js
+++ plugins/dl-imagen.js
@@ -2,12 +2,12 @@ import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 if (!text) return conn.reply(m.chat, `*🤍 Uso Correcto: ${usedPrefix + command} la playa*`, m, rcanal);
 await m.react('🕓');
-let capt = ``
+let capt = `*[ GenesisBot By Angel-OFC ]*`
 const res = await googleImage(text);
 const image = await res.getRandom();
 const link = image;
 const messages = [['Imagen 1', dev, await res.getRandom(),
-[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 5', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 6', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 7', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 8', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 8', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 9', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 10', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+[[]], [[]], [[]], [[]]], ['Imagen 2', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 5', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 6', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 7', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 8', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 8', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 9', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 10', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
 await m.react('✅');
 await conn.sendCarousel(m.chat, '🤍 Buscador - Imagenes', `🔍 Resultado de: ${text}`, text, messages, m, null, fake);
 };
