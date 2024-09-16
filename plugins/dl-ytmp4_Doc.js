ebc0a9bb6e84ac74934464267ded7f3479eefee7
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-16 13:23:58 -0400

    Actualizar dl-ytmp4_Doc.js

--- plugins/dl-ytmp4_Doc.js
+++ plugins/dl-ytmp4_Doc.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, 
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-if (!args || !args[0]) return star.reply(m.chat, 'ðŸš© Ingresa el enlace del vÃ­deo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
+if (!args || !args[0]) return star.reply(m.chat, '*\`Ingresa El link Del documento a descargar 🤍\`*', m, rcanal)
 if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m, rcanal).then(_ => m.react('✅'))
 let q = args[1] || '360p'
 
