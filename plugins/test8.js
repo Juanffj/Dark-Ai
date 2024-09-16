74bacdc2de7fd8f6b4d7027758c4273b363b1788
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-15 20:14:39 -0400

    Actualizar test8.js

--- plugins/test8.js
+++ plugins/test8.js
@@ -120,7 +120,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['imagen <query>'];
 handler.corazones = 2;
 handler.tags = ['buscador'];
-handler.command = /^(image|imagen)$/i;
+handler.command = /^(googleimages)$/i;
 handler.register = true;
 
 export default handler;
