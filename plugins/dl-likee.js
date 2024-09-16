1d6fd7a1f6f3a7324d833db340b1e1deef2c8f55
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-16 13:16:48 -0400

    Actualizar dl-likee.js

--- plugins/dl-likee.js
+++ plugins/dl-likee.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 Ingrese la url de un video de *Likee*.', m, rcanal)
+if (!text) return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*', m, rcanal)
 await m.react('🕓')
 try {
 let app = await fetch(`https://apis-starlights-team.koyeb.app/starlight/like-downloader?url=${text}`, { headers: { 'Content-Type': 'application/json' }})
