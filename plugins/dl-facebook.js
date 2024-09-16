4bdd0d871d291a578362b642b33b911e8bccdaea
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-16 13:13:36 -0400

    Actualizar dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -2,7 +2,7 @@
 
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar\`*', m, fake);
+    return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*', m, fake);
   }
 
   await m.react('🕒');
