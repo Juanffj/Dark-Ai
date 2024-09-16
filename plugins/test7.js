9a7324ab9f67712604bf3ab0f4b7c5b311593ad3
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-15 21:50:38 -0400

    Actualizar test7.js

--- plugins/test7.js
+++ plugins/test7.js
@@ -1,15 +1,13 @@
 import axios from 'axios'
 
-const query = [
-  'canciones%20famosas%20latinoamerica', 
+const query = [  'canciones%20famosas%20latinoamerica', 
   'musica%20latina%20popular',
   'video%20musical%20latino',
   'canciones%20famosas%20de%20latinoamerica',
   'musica%20latina',
   'videos%20de%20canciones%20latinas',
   'canciones%20viral%20latino',
-  'hits%20latinos'
-]
+  'hits%20latinos']
 
 let handler = async (m, {
     conn,
@@ -18,17 +16,17 @@ let handler = async (m, {
     usedPrefix,
     command
 }) => {
-  m.reply('Buscando video...')
-  tiktoks(`${query.getRandom()}`).then(a => {
-    let cap = a.title
-    conn.sendMessage(m.chat, {video: {url: a.no_watermark}, caption: cap}, {quoted: m})
-  }).catch(err => {
-    m.reply('Error al obtener el video.')
-  })
+ m.reply(wait)
+tiktoks(`${query.getRandom()}`).then(a => {
+let cap = a.title
+conn.sendMessage(m.chat, {video: {url: a.no_watermark}, caption: cap}, {quoted: m})
+}).catch(err => {
+m.reply(eror)
+})
 }
 handler.help = ['tiktokrandom']
 handler.tags = ['dl']
-handler.command = /^(tiktokmusic)$/i
+handler.command = /^(tiktokrandom|ttrandom)$/i
 handler.limit = true 
 handler.register = true
 
@@ -41,4 +39,36 @@ async function tiktoks(query) {
         method: 'POST',
         url: 'https://tikwm.com/api/feed/search',
         headers: {
-          'Content-Type'
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Cookie': 'current_language=en',
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36'
+        },
+        data: {
+          keywords: query,
+          count: 10,
+          cursor: 0,
+          HD: 1
+        }
+      });
+      const videos = response.data.data.videos;
+      if (videos.length === 0) {
+        reject("Tidak ada video ditemukan.");
+      } else {
+        const gywee = Math.floor(Math.random() * videos.length);
+        const videorndm = videos[gywee]; 
+
+        const result = {
+          title: videorndm.title,
+          cover: videorndm.cover,
+          origin_cover: videorndm.origin_cover,
+          no_watermark: videorndm.play,
+          watermark: videorndm.wmplay,
+          music: videorndm.music
+        };
+        resolve(result);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
