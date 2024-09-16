d99f43c8585da2aea375efd4cb5138fa654ee7ff
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-15 20:56:07 -0400

    Actualizar test8.js

--- plugins/test8.js
+++ plugins/test8.js
@@ -13,6 +13,10 @@ var handler = async (m, { conn, isPrems, text }) => {
   if (text.startsWith('canjear ')) {
     const code = text.slice(8).trim(); // Extrae el código de canje del texto
     
+    if (!code) {
+      return conn.reply(m.chat, '❌ Por favor, proporciona un código de canje.', m);
+    }
+    
     if (usedCodes.has(code)) {
       return conn.reply(m.chat, '❌ Este código de canje ya ha sido utilizado.', m);
     }
@@ -29,16 +33,16 @@ var handler = async (m, { conn, isPrems, text }) => {
     }
   }
 
+  // Código para el comando 'daily' y 'claim'
   let coin = pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99, 100, 110, 120, 130, 600, 1000, 1500]) * 1;
   let exp = pickRandom([500, 600, 700, 800, 900, 999, 1000, 1300, 1500, 1800]) * 1;
   let exppremium = pickRandom([1000, 1500, 1800, 2100, 2500, 2900, 3300, 3600, 4000, 4500]) * 1;
   let d = Math.floor(Math.random() * 30);
-  
+
   global.db.data.users[m.sender].diamond += d;
   global.db.data.users[m.sender].money += d;
-  
   global.db.data.users[m.sender].exp += isPrems ? exppremium : exp;
-  
+
   conn.reply(m.chat, `🎁 *Recompensa Diaria*
 
 Recursos:
