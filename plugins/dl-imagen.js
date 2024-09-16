0e4f69fe5026c08e5c415fb954cd6e7f27eb26b2
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-16 17:29:00 -0400

    Actualizar dl-imagen.js

--- plugins/dl-imagen.js
+++ plugins/dl-imagen.js
@@ -1,20 +1,21 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*\`Ingresa El texto que deseas buscar 🤍\`*`;
-  conn.reply(m.chat, '🤍 *Descargando su imagen...*', m, {
-  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-  title: packname,
-  body: wm,
-  previewType: 0, thumbnail: icons,
-  sourceUrl: canal }}})
-  const res = await googleImage(text);
-  const image = await res.getRandom();
-  const link = image;
-  conn.sendFile(m.chat, link, 'error.jpg', `*🔎 Resultado De: ${text}*\n> ${textbot}`, m, null, rcanal);
+if (!text) return conn.reply(m.chat, `*🤍 Uso Correcto: ${usedPrefix + command} la playa*`, m, rcanal);
+conn.reply(m.chat, '🤍 *Descargando su imagen...*', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: canal }}})
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, '🚩 Buscador - Imagenes', `🔍 Resultado de: ${text}`, text, messages, m);
 };
 handler.help = ['imagen <query>'];
-handler.corazones = 2
-handler.tags = ['buscador'];
-handler.command = /^(image|imagen)$/i;
+handler.tags = ['buscador', 'tools', 'descargas'];
+handler.command = ['image', 'imagen'];
 handler.register = true;
 export default handler;
