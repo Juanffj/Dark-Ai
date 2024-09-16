55ec0a45002a9d39720c2650e274f6293cffcc3b
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-16 13:20:07 -0400

    Actualizar dl-tiktok2.js

--- plugins/dl-tiktok2.js
+++ plugins/dl-tiktok2.js
@@ -3,7 +3,7 @@ import FormData from "form-data";
 import cheerio from "cheerio";
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, '🤍 Ingresa un link junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* linkvideo`, m, rcanal)
+  if (!text) return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*', m, rcanal)
     try {
 let data = await tiktokdl(text)
 console.log(data)
