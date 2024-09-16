8b75adcb3b5ea9bcbf746d5a49e9c308d1a38a70
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-15 22:33:13 -0400

    Actualizar test5.js

--- plugins/test5.js
+++ plugins/test5.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) return conn.reply(m.chat, '*ingresa el texto de lo que quieres buscar en tiktok*', m);
+await m.react('🕒');
   
 
 try {
@@ -15,11 +16,13 @@ let info = json[Math.floor(json.length * Math.random())];
 
 if (info) {
 await conn.sendMessage(m.chat, { video: { url: info.nowm }, caption: info.url }, { quoted: m });
+await m.react('✅');
 } else {
 conn.reply('error :v')
 }
 } catch {
-conn.reply('error :v')
+conn.reply('error :v');
+await m.react('✖️')
 }
 };
 
