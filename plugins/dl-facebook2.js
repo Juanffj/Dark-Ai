7191ce848e17ab231d5db082efe9841cef64118a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-15 22:29:38 -0400

    Actualizar plugins

---
+++ plugins/dl-facebook2.js
@@ -0,0 +1,14 @@
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `Use example ${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*`
+await m.react('🕓');
+const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+await m.react('✅');
+for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*\`[ FACEBOOK VIDEO ]\`*`, m, null, rcanal)
+}
+handler.help = ['facebook2'].map(v => v + ' <url>')
+handler.corazones = 2
+handler.tags = ['dl']
+handler.command = ['facebook2']
+handler.exp = 35
+export default handler
