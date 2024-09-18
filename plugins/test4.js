7bee71502ea6bf7961ecbef1fbd10e2700ce385e
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-17 21:34:38 -0400

    Actualizar test4.js

--- plugins/test4.js
+++ plugins/test4.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let pp = await conn.profilePictureUrl(who).catch(_ => icons.getRandom())
 let alok = {
         key : {
                           participant : '0@s.whatsapp.net'
