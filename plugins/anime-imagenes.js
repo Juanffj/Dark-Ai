737c958c577668934107b8bfcd4a9f555b851402
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-17 21:48:16 -0400

    Actualizar anime-imagenes.js

--- plugins/anime-imagenes.js
+++ plugins/anime-imagenes.js
@@ -1,4 +1,4 @@
-/* import axios from 'axios';
+import axios from 'axios';
 const handler = async (m, {command, conn, usedPrefix}) => {
 const res = (await axios.get(`https://raw.githubusercontent.com/WOTCHITA/YaemoriBot-MD/master/src/JSON/anime-${command}.json`)).data;
 const haha = await res[Math.floor(res.length * Math.random())];
@@ -6,4 +6,4 @@ conn.sendFile(m.chat, haha, 'error.jpg', `🤍 *${command}*`, m, null, rcanal);
 };
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
 handler.tags = ['anime'];
-export default handler; */
+export default handler;
