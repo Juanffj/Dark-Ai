4088477d50b93724191af8cfc6f5c6ea21efaa9f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-17 20:27:37 -0400

    Actualizar test5.js

--- plugins/test5.js
+++ plugins/test5.js
@@ -8,7 +8,7 @@ import Jimp from "jimp";
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) return m.reply(`• *Ejemplo:* ${usedPrefix + command} elaina edit`);
 
-    await m.reply('*_`Cargando`_*');
+    await m.react('🕒');
 
     async function createImage(img) {
         const { imageMessage } = await generateWAMessageContent({
@@ -86,6 +86,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         }
     }, {});
 
+    await m.react('✅');
     await conn.relayMessage(m.chat, bot.message, { messageId: bot.key.id });
 }
 
