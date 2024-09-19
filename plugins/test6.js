6035c26d5fbeb1262ca3e7e10bc35d1246dd0e3d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-19 19:42:01 -0400

    Actualizar test6.js

--- plugins/test6.js
+++ plugins/test6.js
@@ -1,8 +1,6 @@
-import { URL_REGEX } from '@whiskeysockets/baileys'
-import { fileTypeFromBuffer } from 'file-type'
-import { Pixiv } from '@ibaraki-douji/pixivts'
-const pixiv = new Pixiv()
-import { prepareWAMessageMedia, generateWAMessageFromContent, proto } from '@whiskeysockets/baileys';
+import { URL_REGEX } from '@whiskeysockets/baileys';
+import { fileTypeFromBuffer } from 'file-type';
+import { Pixiv } from '@ibaraki-douji/pixivts';
 const pixiv = new Pixiv();
 
 let handler = async (m, { conn, text }) => {
@@ -11,70 +9,27 @@ let handler = async (m, { conn, text }) => {
 
     try {
         let res = await pixivDl(text);
-        if (!res) return m.reply('No se encontraron resultados.');
+        if (!res) return m.reply('Resultados no encontrados.');
 
-        let results = [];
+        const messages = [];
         for (let i = 0; i < res.media.length; i++) {
-            const mediaMessage = await prepareWAMessageMedia({ image: res.media[i] }, { upload: conn.waUploadToServer });
-            results.push({
-                body: proto.Message.InteractiveMessage.Body.fromObject({
-                    text: `*» Nombre:* ${res.caption}\n*» Subido por:* ${res.artist}\n*» Tags:* ${res.tags.join(', ')}`
-                }),
-                footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: '*[ Pixiv Search ]*' }),
-                header: proto.Message.InteractiveMessage.Header.fromObject({
-                    title: `*Imagen ${i + 1}*`,
-                    hasMediaAttachment: true,
-                    imageMessage: mediaMessage.imageMessage
-                }),
-                nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] }),
-            });
+            let caption = i == 0 ? `*» Nombre :* ${res.caption}\n*» Subido por :* ${res.artist}\n*» Tags* : ${res.tags.join(', ')}` : '';
+            let mime = (await fileTypeFromBuffer(res.media[i])).mime;
+            messages.push([`Imagen ${i + 1}`, caption, res.media[i], [[]], [[]], [[]], [[]]]);
         }
 
-        // Enviar el mensaje en carrusel
-        const messageContent = generateWAMessageFromContent(m.chat, {
-            viewOnceMessage: {
-                message: {
-                    messageContextInfo: {
-                        deviceListMetadata: {},
-                        deviceListMetadataVersion: 2
-                    },
-                    interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-                        body: proto.Message.InteractiveMessage.Body.create({
-                            text: `🤍 *Resultados de búsqueda para:* \`${text}\``
-                        }),
-                        footer: proto.Message.InteractiveMessage.Footer.create({
-                            text: "_\`Pixiv Search\`_"
-                        }),
-                        header: proto.Message.InteractiveMessage.Header.create({
-                            hasMediaAttachment: false
-                        }),
-                        carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
-                            cards: results
-                        }),
-                        contextInfo: {
-                            mentionedJid: [m.sender],
-                            forwardingScore: 999,
-                            isForwarded: true,
-                        }
-                    })
-                }
-            }
-        }, {
-            quoted: m
-        });
-
-        await conn.sendMessage(m.chat, messageContent, { quoted: m });
         await m.react('✅');
+        await conn.sendCarousel(m.chat, '🤍 Buscador - Imágenes', `🔍 Resultado de: ${text}`, text, messages, fakegif3);
 
     } catch {
         await m.react('✖️');
-        m.reply('Ocurrió un error.');
+        m.reply('Error al procesar la solicitud.');
     }
 };
 
 handler.help = ['pixiv *<búsqueda>*'];
 handler.tags = ['search'];
-handler.command = /^(pixivthg|pixivdl)$/i;
+handler.command = /^(pixivvv|pixivdl)$/i;
 handler.register = true;
 
 export default handler;
@@ -84,7 +39,7 @@ async function pixivDl(query) {
         if (!/https:\/\/www.pixiv.net\/en\/artworks\/[0-9]+/i.test(query)) return null;
         query = query.replace(/\D/g, '');
         let res = await pixiv.getIllustByID(query).catch(() => null);
-        if (!res) return null; // Cambiar a 'null' para evitar enviar un mensaje si no se encuentra nada
+        if (!res) return null;
         let media = [];
         for (let x = 0; x < res.urls.length; x++) media.push(await pixiv.download(new URL(res.urls[x].original)));
         return {
@@ -92,7 +47,7 @@ async function pixivDl(query) {
         };
     } else {
         let res = await pixiv.getIllustsByTag(query);
-        if (!res.length) return null; // Cambiar a 'null' para evitar enviar un mensaje si no se encuentra nada
+        if (!res.length) return null;
         res = res[~~(Math.random() * res.length)].id;
         res = await pixiv.getIllustByID(res);
         let media = [];
