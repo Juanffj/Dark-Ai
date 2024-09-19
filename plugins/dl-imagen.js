6e2d1bd782eda47b1ae9f61e164126e1313c313d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-19 19:36:48 -0400

    Actualizar dl-imagen.js

--- plugins/dl-imagen.js
+++ plugins/dl-imagen.js
@@ -1,18 +1,59 @@
-import {googleImage} from '@bochilteam/scraper';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `*🤍 Uso Correcto: ${usedPrefix + command} la playa*`, m, rcanal);
-await m.react('🕓');
-let capt = `*[ GenesisBot By Angel-OFC ]*`
-const res = await googleImage(text);
-const image = await res.getRandom();
-const link = image;
-const messages = [['Imagen 1', capt, await res.getRandom(),
-[[]], [[]], [[]], [[]]], ['Imagen 2', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 5', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 6', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 7', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 8', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 9', capt, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 10', capt, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-await m.react('✅');
-await conn.sendCarousel(m.chat, '🤍 Buscador - Imagenes', `🔍 Resultado de: ${text}`, text, messages, fakegif3);
+import { URL_REGEX } from '@whiskeysockets/baileys';
+import { fileTypeFromBuffer } from 'file-type';
+import { Pixiv } from '@ibaraki-douji/pixivts';
+const pixiv = new Pixiv();
+
+let handler = async (m, { conn, text }) => {
+    if (!text) return m.reply('🤍 *`INGRESA NOMBRE DE LA IMG`*');
+    await m.react('🕓');
+
+    try {
+        let res = await pixivDl(text);
+        if (!res) return m.reply('Resultados no encontrados.');
+
+        const messages = [];
+        for (let i = 0; i < res.media.length; i++) {
+            let caption = i == 0 ? `*» Nombre :* ${res.caption}\n*» Subido por :* ${res.artist}\n*» Tags* : ${res.tags.join(', ')}` : '';
+            let mime = (await fileTypeFromBuffer(res.media[i])).mime;
+            messages.push([`Imagen ${i + 1}`, caption, res.media[i], [[]], [[]], [[]], [[]]]);
+        }
+
+        await m.react('✅');
+        await conn.sendCarousel(m.chat, '🤍 Buscador - Imágenes', `🔍 Resultado de: ${text}`, text, messages, fakegif3);
+
+    } catch {
+        await m.react('✖️');
+        m.reply('Error al procesar la solicitud.');
+    }
 };
-handler.help = ['imagen <query>'];
-handler.tags = ['buscador', 'tools', 'descargas'];
-handler.command = ['image', 'imagen'];
+
+handler.help = ['pixiv *<búsqueda>*'];
+handler.tags = ['search'];
+handler.command = /^(pixivtgf|pixivdl)$/i;
 handler.register = true;
+
 export default handler;
+
+async function pixivDl(query) {
+    if (query.match(URL_REGEX)) {
+        if (!/https:\/\/www.pixiv.net\/en\/artworks\/[0-9]+/i.test(query)) return null;
+        query = query.replace(/\D/g, '');
+        let res = await pixiv.getIllustByID(query).catch(() => null);
+        if (!res) return null;
+        let media = [];
+        for (let x = 0; x < res.urls.length; x++) media.push(await pixiv.download(new URL(res.urls[x].original)));
+        return {
+            artist: res.user.name, caption: res.title, tags: res.tags.tags.map(v => v.tag), media
+        };
+    } else {
+        let res = await pixiv.getIllustsByTag(query);
+        if (!res.length) return null;
+        res = res[~~(Math.random() * res.length)].id;
+        res = await pixiv.getIllustByID(res);
+        let media = [];
+        for (let x = 0; x < res.urls.length; x++) media.push(await pixiv.download(new URL(res.urls[x].original)));
+        return {
+            artist: res.user.name, caption: res.title, tags: res.tags.tags.map(v => v.tag), media
+        };
+    }
+}
