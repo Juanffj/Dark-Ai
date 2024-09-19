c4afc09eba5ee6a75018e3f42c8f3b108bc58f59
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-19 17:36:37 -0400

    Actualizar test4.js

--- plugins/test4.js
+++ plugins/test4.js
@@ -5,7 +5,7 @@ import {
   prepareWAMessageMedia
 } from "@whiskeysockets/baileys";
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { command, conn }) => {
   await m.react('🕒');
 
   try {
@@ -19,33 +19,57 @@ let handler = async (m, { conn }) => {
       if (!res.ok) throw new Error('Error al obtener imagen de waifu');
       const json = await res.json();
       if (!json.url) throw new Error('No se encontró la URL de la imagen');
-      
+
       // Preparar el mensaje de la imagen
       const mediaMessage = await prepareWAMessageMedia({ image: json.url }, { upload: conn.waUploadToServer });
       results.push({
         body: proto.Message.InteractiveMessage.Body.fromObject({ text: null }),
         footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: '*[ GenesisBot By Angel-OFC ]*' }),
         header: proto.Message.InteractiveMessage.Header.fromObject({
-          title: '*Imagen de Waifu*',
+          title: `*\`Imagen De:\`* ${command}`,
           hasMediaAttachment: true,
           imageMessage: mediaMessage.imageMessage
         }),
+        nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
       });
     }
 
     // Enviar el mensaje en carrusel
     const messageContent = generateWAMessageFromContent(m.chat, {
-      interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-        body: proto.Message.InteractiveMessage.Body.create({
-          text: `🤍 Waifus para ti 🤍`
-        }),
-        footer: proto.Message.InteractiveMessage.Footer.create({
-          text: "_\`ᴀ\` \`ɴ\` \`ɪ\` \`ᴍ\` \`ᴇ\` - \`2\` \`0\` \`2\` \`4\`_"
-        }),
-        carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
-          cards: results
-        }),
-      })
+      viewOnceMessage: {
+        message: {
+          messageContextInfo: {
+            deviceListMetadata: {},
+            deviceListMetadataVersion: 2
+          },
+          interactiveMessage: proto.Message.InteractiveMessage.fromObject({
+            body: proto.Message.InteractiveMessage.Body.create({
+              text: `🤍 \`${command}\` 🤍`
+            }),
+            footer: proto.Message.InteractiveMessage.Footer.create({
+              text: "_\`ᴀ\` \`ɴ\` \`ɪ\` \`ᴍ\` \`ᴇ\` - \`2\` \`0\` \`2\` \`4\`_"
+            }),
+            header: proto.Message.InteractiveMessage.Header.create({
+              hasMediaAttachment: false
+            }),
+            carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({
+              cards: results
+            }),
+            contextInfo: {
+              mentionedJid: [m.sender],
+              forwardingScore: 999,
+              isForwarded: true,
+              forwardedNewsletterMessageInfo: {
+                newsletterJid: "120363220939514640@newsletter",
+                newsletterName: "𝑮𝒆𝒏𝒆𝒔𝒊𝒔-𝑩𝒐𝒕 - 𝑪𝒉𝒂𝒏𝒏𝒆𝒍",
+                serverMessageId: 143
+              }
+            }
+          })
+        }
+      }
+    }, {
+      quoted: m
     });
 
     await conn.sendMessage(m.chat, messageContent, { quoted: m });
