58966eec90fbd77c0fb4ef01abb52102303cb57c
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-18 20:51:41 -0400

    Actualizar test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -69,7 +69,7 @@ let handler = async(m, { conn, usedPrefix, command, text }) => {
             text: `Resultados de *${text}*`
           }),
           footer: proto.Message.InteractiveMessage.Footer.create({
-            text: '_\`ᴛ\` \`ɪ\`\`ᴋ\` \`ᴛ\` \`ᴏ\`\`ᴋ\`\`-\` \`ꜱ\` \`ᴇ\` \`ᴀ\` \`ʀ\` \`ᴄ\` \`ʜ\`_',
+            text: '_\`ᴛ\` \`ᴛ\`-\` \`ꜱ\` \`ᴇ\` \`ᴀ\` \`ʀ\` \`ᴄ\` \`ʜ\`_',
           }),
           header: proto.Message.InteractiveMessage.Header.create({
             hasMediaAttachment: false
