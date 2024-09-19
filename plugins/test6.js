96a43564cdaa66148d1e92a2ae5e15996de1242d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-19 19:02:45 -0400

    Actualizar test6.js

--- plugins/test6.js
+++ plugins/test6.js
@@ -5,7 +5,7 @@ import search from 'yt-search';
 
 // Función para buscar en Spotify a través de un proxy o API pública
 async function spotifySearch(query) {
-    const response = await axios.get(`https://api.spotify.com/v1/search?q=`, {
+    const response = await axios.get(`https://accounts.spotify.com/api/token`, {
         params: {
             q: query,
             type: 'track',
