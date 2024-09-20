72d9a806545a83a9939e8313c60903f42224a7fa
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-20 15:01:38 -0400

    Actualizar info-grupos.js

--- plugins/info-grupos.js
+++ plugins/info-grupos.js
@@ -1,6 +1,6 @@
 
 let handler = async (m, { conn, command, usedPrefix }) => {
-let pp = 'https://i.ibb.co/YcdFyft/file.jpg'
+let pp = 'https://tinyurl.com/275dvpbo'
 m.react('🤍')
 let name = await conn.getName(m.sender)
 let _uptime = process.uptime() * 1000
