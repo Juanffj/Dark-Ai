3feea5a2eacf2e6ac785ffdde751ce597fafd437
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-20 23:12:19 -0400

    Actualizar dl-facebook2.js

--- plugins/dl-facebook2.js
+++ plugins/dl-facebook2.js
@@ -1,18 +1,3 @@
-/* import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `*\`Ingresa El link Del vídeo a descargar 🤍\`*`
-await m.react('🕓');
-const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-await m.react('✅');
-for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*\`[ FACEBOOK VIDEO ]\`*`, m, null, rcanal)
-}
-handler.help = ['facebook2'].map(v => v + ' <url>')
-handler.corazones = 2
-handler.tags = ['dl']
-handler.command = ['facebook2']
-handler.exp = 35
-export default handler */
-
 import { facebookdl, facebookdlv2 } from '@bochilteam/scraper';
 
 let handler = async (m, { conn, args }) => {
@@ -23,7 +8,6 @@ let handler = async (m, { conn, args }) => {
     
     await m.react('✅');
     
-    // Enviar solo el primer vídeo
     if (result.length > 0) {
         const { url, isVideo } = result[0];
         await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*\`[ FACEBOOK VIDEO ]\`*`, m, null, rcanal);
@@ -35,7 +19,7 @@ let handler = async (m, { conn, args }) => {
 handler.help = ['facebook2'].map(v => v + ' <url>');
 handler.corazones = 2;
 handler.tags = ['dl'];
-handler.command = ['facebook2'];
+handler.command = ['facebook2','fb2'];
 handler.exp = 35;
 
 export default handler;
