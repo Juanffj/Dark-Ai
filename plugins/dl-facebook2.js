81116c07e125a9542ecc4e0d108298615355b27c
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-20 23:10:50 -0400

    Actualizar dl-facebook2.js

--- plugins/dl-facebook2.js
+++ plugins/dl-facebook2.js
@@ -1,4 +1,4 @@
-import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+/* import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `*\`Ingresa El link Del vídeo a descargar 🤍\`*`
 await m.react('🕓');
@@ -11,4 +11,31 @@ handler.corazones = 2
 handler.tags = ['dl']
 handler.command = ['facebook2']
 handler.exp = 35
-export default handler
+export default handler */
+
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper';
+
+let handler = async (m, { conn, args }) => {
+    if (!args[0]) throw `*\`Ingresa El link Del vídeo a descargar 🤍\`*`;
+    
+    await m.react('🕓');
+    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]));
+    
+    await m.react('✅');
+    
+    // Enviar solo el primer vídeo
+    if (result.length > 0) {
+        const { url, isVideo } = result[0];
+        await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*\`[ FACEBOOK VIDEO ]\`*`, m, null, rcanal);
+    } else {
+        throw `*\`No se encontró ningún vídeo.\`*`;
+    }
+}
+
+handler.help = ['facebook2'].map(v => v + ' <url>');
+handler.corazones = 2;
+handler.tags = ['dl'];
+handler.command = ['facebook2'];
+handler.exp = 35;
+
+export default handler;
