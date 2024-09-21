d1875830310d157a0b057ee3894f7e1622863da6
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-20 22:39:24 -0400

    Actualizar test7.js

--- plugins/test7.js
+++ plugins/test7.js
@@ -3,7 +3,7 @@ import cheerio from 'cheerio';
 
 let handler = async (m, { args, conn }) => { 
     if (!args[0]) {
-        return conn.reply(m.chat, '*\`Ingresa el link de la publicación a descargar 🤍\`*', m, fake);
+        return conn.reply(m.chat, '*\`Ingresa el link del vídeo a descargar 🤍\`*', m, fake);
     }
 
     try {
@@ -11,33 +11,28 @@ let handler = async (m, { args, conn }) => {
         
         const response = await axios.get(args[0]);
         const $ = cheerio.load(response.data);
-
-        // Busca la imagen
-        const imgUrl = $('meta[property="og:image"]').attr('content');
-        // Busca el video
+        
+        // Busca la URL del vídeo
         const videoUrl = $('meta[property="og:video"]').attr('content');
 
-        if (imgUrl) {
-            await new Promise(resolve => setTimeout(resolve, 2000));
-            await m.react('✅');
-            await conn.sendFile(m.chat, imgUrl, 'imagen.jpg', 'Imagen descargada de Threads', null, m);
-        } else if (videoUrl) {
-            await new Promise(resolve => setTimeout(resolve, 2000));
-            await m.react('✅');
-            await conn.sendFile(m.chat, videoUrl, 'video.mp4', 'Vídeo descargado de Threads', null, m);
-        } else {
-            throw new Error('No se encontró contenido multimedia.');
+        if (!videoUrl) {
+            throw new Error('No se encontró el vídeo.');
         }
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        
+        await m.react('✅');
+        await conn.sendFile(m.chat, videoUrl, 'video.mp4', 'Vídeo descargado de Threads', null, m);
     } catch (error) {
         console.error(error);
         await m.react('❌');
-        return conn.reply(m.chat, '*\`Error al descargar el contenido. Asegúrate de que el link sea correcto.\`*', m);
+        return conn.reply(m.chat, '*\`Error al descargar el vídeo. Asegúrate de que el link sea correcto.\`*', m);
     }
 }
 
 handler.corazones = 2;
-handler.command = ['threads', 'thdl', 'thread'];
+handler.command = ['threads2'];
 handler.tags = ['dl'];
-handler.help = ['threads <link>'];
+handler.help = ['threads2 <link>'];
 
 export default handler;
