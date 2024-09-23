ef669f0283a623ab24e45e9a36467e8fd8cd2343
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-23 11:54:50 -0400

    Add files via upload

---
+++ plugins/fun-pokedex.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, '🤍 *¿Que Pokémon quieres buscar?.*', m, rcanal)
+await m.react(rwait)
+conn.reply(m.chat, `🤍 *Buscando ${text}*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: canal }}})
+const url = `https://some-random-api.com/pokemon/pokedex?pokemon=${encodeURIComponent(text)}`;
+const response = await fetch(url);
+const json = await response.json();
+if (!response.ok) {
+await m.react(error)
+return conn.reply(m.chat, '🤍 *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, rcanal)}
+const luffypokedex = `🤍 *Pokedex - Información de ${json.name}*\n\n🤍 *Nombre:* ${json.name}\n🤍 *ID:* ${json.id}\n🤍 *Tipo:* ${json.type}\n🤍 *Habilidades:* ${json.abilities}\n*Tamaño:* ${json.height}\n🤍 *Peso:* ${json.weight}\n\n📖 *Descripción:*\n${json.description}\n\n🔍 ¡Encuentra más detalles sobre este Pokémon en la Pokedex! 🔍\n\n🔗 https://www.pokemon.com/es/pokedex/${json.name.toLowerCase()}`
+conn.reply(m.chat, luffypokedex, m, rcanal)
+await m.react(done) }
+
+handler.help = ['pokedex *<pokemon>*']
+handler.tags = ['buscador']
+handler.command = /^pokedex/i
+export default handler
