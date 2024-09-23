8a34a23fb6815c0a19167c6effd41ef8567e9f33
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-23 12:06:25 -0400

    Add files via upload

---
+++ plugins/sticker-qc.js
@@ -0,0 +1,29 @@
+/* Codigo copiado de GataBot-MD */
+
+import { sticker } from '../lib/sticker.js';
+import axios from 'axios';
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ");
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text;
+    } else throw "➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*PONGA UN TEXTO*\n_Ejemplo: .qc Hola_";
+   if (!text) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*PONGA UN TEXTO*\n_Ejemplo: .qc Hola_');
+    const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
+    const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
+    const mishi = text.replace(mentionRegex, '');
+   if (mishi.length > 30) return m.reply('➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*EL TEXTO NO PUEDE TENER MAS DE 30 CARACTERES*');
+m.react('🐈‍⬛') 
+    const pp = await conn.profilePictureUrl(who).catch((_) => 'https://i.ibb.co/rZcyhcL/file.jpg')
+    const nombre = await conn.getName(who)
+    const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
+    const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
+    const buffer = Buffer.from(json.data.result.image, 'base64');
+   let stiker = await sticker(buffer, false, global.packname, global.author);
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `La Mejor Bot 🌟`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
+}
+handler.help = ['qc'];
+handler.tags = ['sticker'];
+handler.command = /^(qc)$/i;
+export default handler;
