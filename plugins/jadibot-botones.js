ab26bc0770bd779e715e757eeaa25b9091c14066
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-23 13:10:55 -0400

    Actualizar jadibot-botones.js

--- plugins/jadibot-botones.js
+++ plugins/jadibot-botones.js
@@ -1,4 +1,4 @@
-let media = 'https://i.ibb.co/3BppnNL/file.jpg'
+let media = 'https://i.ibb.co/pnjKCZK/file.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 await m.react('☁️');
