1f3ec811c31281a7de5b3d627a768ff17836cea5
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-24 17:19:13 -0400

    Actualizar fun-casino.js

--- plugins/fun-casino.js
+++ plugins/fun-casino.js
@@ -22,7 +22,7 @@ cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
 if (args.length < 1) return conn.reply(m.chat, '💣 Ingresa la cantidad de ' + `🤍 *Corazones*` + ' que deseas aportar contra' + ` *GenesisBot-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
-if (user.cookies >= count * 1) {
+if (user.corazones >= count * 1) {
 user.corazones -= count * 1
 if (Aku > Kamu) {
 conn.reply(m.chat, '`💣 Veamos que numeros tienen!`\n\n'+ `➠ *GenesisBot-MD* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🤍 Corazones.`.trim(), m, rcanal)
